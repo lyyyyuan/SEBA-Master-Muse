96603fb95c159849911f0da153e28d6745770300
ga84how <yuan1994.liu@tum.de>
2018-06-17 10:15:57 +0200

    add store page

--- frontend/src/views/SearchPageView.js
+++ frontend/src/views/SearchPageView.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import queryString from 'query-string';
 import SearchPage from '../components/SearchPage/SearchPage';
 
@@ -6,13 +6,12 @@ import SearchPage from '../components/SearchPage/SearchPage';
 class SearchPageView extends Component {
     constructor(props) {
         super(props);
-        console.log(this.props.location);
     }
-    
+
     render() {
         return (
             <div>
-                <SearchPage location={this.props.location} query={queryString.parse(this.props.location.search)} />
+                <SearchPage location={this.props.location} query={queryString.parse(this.props.location.search)}/>
             </div>
         );
     }
