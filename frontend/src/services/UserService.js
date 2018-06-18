c3984eeb7043b441507a6a39276bd7fb2a6669fd
ZhangHanming <zhang.hanming.official@gmail.com>
2018-06-18 19:34:20 +0200

    add babel polyfill

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,6 +1,7 @@
 "use strict";
 
 import HttpService from "./HttpService";
+import AsyncHttpService from './AsyncHttpService';
 
 export default class UserService {
 
@@ -22,16 +23,23 @@ export default class UserService {
         });
     }
 
-    static login(user, pass) {
-        return new Promise((resolve, reject) => {
-            HttpService.post(`${UserService.baseURL()}/login`, {
-                username: user,
-                password: pass
-            }, function(data) {
-                resolve(data);
-            }, function(textStatus) {
-                reject(textStatus);
-            });
+    // static login(user, pass) {
+    //     return new Promise((resolve, reject) => {
+    //         HttpService.post(`${UserService.baseURL()}/login`, {
+    //             username: user,
+    //             password: pass
+    //         }, function(data) {
+    //             resolve(data);
+    //         }, function(textStatus) {
+    //             reject(textStatus);
+    //         });
+    //     });
+    // }
+
+    static async login(user, pass) {
+        return await AsyncHttpService.post(`${UserService.baseURL()}/login`, {
+            username: user,
+            password: pass
         });
     }
 
