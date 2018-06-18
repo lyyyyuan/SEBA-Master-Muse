7ee2ac06ff9796a00718cca6d2c2a81433237077
ZhangHanming <zhang.hanming.official@gmail.com>
2018-06-18 19:17:45 +0200

    add http service async wrapper

---
+++ frontend/src/services/AsyncHttpService.js
@@ -0,0 +1,55 @@
+'use strict';
+
+import HttpService from './HttpService';
+import defer from 'defer-promise';
+import qs from 'query-string';
+
+// a httpservice wrap to support async/await
+export default class AsyncHttpService {
+    static get = async (url, params) => {
+        const deferred = defer();
+        HttpService.get(
+            `${url}?${qs.stringify(params)}`,
+            res => deferred.resolve(res),
+            err => deferred.reject(err)
+        );
+
+        return deferred.promise;
+    }
+
+    static post = async (url, data) => {
+        const deferred = defer();
+        HttpService.post(
+            url,
+            data,
+            res => deferred.resolve(res),
+            err => deferred.reject(err)
+        );
+
+        return deferred.promise;
+    }
+
+    static put = async (url, data) => {
+        const deferred = defer();
+        HttpService.put(
+            url,
+            data,
+            res => deferred.resolve(res),
+            err => deferred.reject(err)
+        );
+
+        return deferred.promise;
+    }
+
+    static delete = async (url, data) => {
+        const deferred = defer();
+        HttpService.remove(
+            url,
+            data,
+            res => deferred.resolve(res),
+            err => deferred.reject(err)
+        );
+
+        return deferred.promise;
+    }
+}
